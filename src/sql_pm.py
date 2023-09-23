c088e8b1a61904c024197e049308955d75c21160
ahmetcucar <acucar@uw.edu>
2023-09-22 22:46:17 -0700

    finish tests

--- src/sql_pm.py
+++ src/sql_pm.py
@@ -111,6 +111,10 @@ class SqlitePasswordManager(password_manager.PasswordManager):
             print("Wrong username or password!")
             return False
 
+        if old_password == new_password:
+            print("Old and new passwords are the same!")
+            return False
+
         return self.delete_credentials(username) and self.add_credentials(username, new_password)
 
 
---
+++ test/test.py
@@ -0,0 +1,101 @@
+import sys
+import os
+import unittest
+
+# Add parent directory to path and import json_pm
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+from src import json_pm, sql_pm
+
+
+# test with json_pm.py
+class TestJSON(unittest.TestCase):
+
+    def test_add_credentials(self):
+        pm = json_pm.JsonPasswordManager("data/test.json")
+        self.assertTrue(pm.add_credentials("user1", "test"))
+        self.assertFalse(pm.add_credentials("user1", "test"))
+        pm.wipe_credentials()
+
+    def test_delete_credentials(self):
+        pm = json_pm.JsonPasswordManager("data/test.json")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.delete_credentials("user1"))
+        self.assertFalse(pm.delete_credentials("user1"))
+        self.assertFalse(pm.delete_credentials("user2"))
+        pm.wipe_credentials()
+
+    def test_change_password(self):
+        pm = json_pm.JsonPasswordManager("data/test.json")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.change_password("user1", "test", "test2"))
+        self.assertFalse(pm.change_password("user1", "test", "test2"))
+        self.assertFalse(pm.change_password("user1", "test2", "test2"))
+        self.assertFalse(pm.change_password("user2", "test", "test2"))
+        pm.wipe_credentials()
+
+    def test_wipe_credentials(self):
+        pm = json_pm.JsonPasswordManager("data/test.json")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.wipe_credentials())
+
+    def test_verify_credentials(self):
+        pm = json_pm.JsonPasswordManager("data/test.json")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.verify_credentials("user1", "test"))
+        self.assertFalse(pm.verify_credentials("user1", "test2"))
+        self.assertFalse(pm.verify_credentials("user2", "test"))
+
+# test with sqlite_pm.py
+class TestSQL(unittest.TestCase):
+
+    def test_add_credentials(self):
+        if os.path.exists("data/test.db"):
+            os.remove("data/test.db")
+        pm = sql_pm.SqlitePasswordManager("data/test.db")
+        self.assertTrue(pm.add_credentials("user1", "test"))
+        pm.wipe_credentials()
+
+    def test_delete_credentials(self):
+        if os.path.exists("data/test.db"):
+            os.remove("data/test.db")
+        pm = sql_pm.SqlitePasswordManager("data/test.db")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.delete_credentials("user1"))
+        self.assertFalse(pm.delete_credentials("user1"))
+        self.assertFalse(pm.delete_credentials("user2"))
+        pm.wipe_credentials()
+
+    def test_change_password(self):
+        if os.path.exists("data/test.db"):
+            os.remove("data/test.db")
+        pm = sql_pm.SqlitePasswordManager("data/test.db")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.change_password("user1", "test", "test2"))
+        self.assertFalse(pm.change_password("user1", "test", "test2"))
+        self.assertFalse(pm.change_password("user1", "test2", "test2"))
+        self.assertFalse(pm.change_password("user2", "test", "test2"))
+        pm.wipe_credentials()
+
+    def test_wipe_credentials(self):
+        if os.path.exists("data/test.db"):
+            os.remove("data/test.db")
+        pm = sql_pm.SqlitePasswordManager("data/test.db")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.wipe_credentials())
+
+    def test_verify_credentials(self):
+        if os.path.exists("data/test.db"):
+            os.remove("data/test.db")
+        pm = sql_pm.SqlitePasswordManager("data/test.db")
+        pm.add_credentials("user1", "test")
+        self.assertTrue(pm.verify_credentials("user1", "test"))
+        self.assertFalse(pm.verify_credentials("user1", "test2"))
+        self.assertFalse(pm.verify_credentials("user2", "test"))
+
+if __name__ == "__main__":
+    if os.path.exists("data/test.db"):
+        os.remove("data/test.db")
+    unittest.main()
+
